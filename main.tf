076e17e9de908b75ab50302a6604bed029774566
swatishri92 <163643583+swatishri92@users.noreply.github.com>
2024-04-03 04:16:28 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+
+ required_providers {
+
+  docker = {
+
+   source = "kreuzwerker/docker"
+
+   version = "2.16.0"
+
+  }
+
+ }
+
+}
+
+
+
+provider "docker" {
+
+ host = "unix:///var/run/docker.sock"
+
+}
+
+
+
+# Pulls the image
+
+resource "docker_image" "nginx" {
+
+ name = "nginx:latest"
+
+}
+
+
+
+# Create a container
+
+resource "docker_container" "foo" {
+
+ image = docker_image.nginx.latest
+
+ name = "foo"
+
+}
+
